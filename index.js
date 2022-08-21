217f2f3e539df2fa97014cb80a72dcf410a175fc
Huy Chu <chuhuyrpi@gmail.com>
2022-08-21 16:13:32 -0400

    Maze generation function

--- index.js
+++ index.js
@@ -36,12 +36,59 @@ const horzWalls = Array(row - 1)
   .fill(null)
   .map(() => Array(col).fill(false));
 
-console.log(mazeArray);
-console.log(vertWalls);
-console.log(horzWalls);
+// Function to generate maze
+const stepThroughCell = (currRow, currCol) => {
+  // If i have visted cell at [row,col], return
+  if (mazeArray[currRow][currCol]) {
+    return;
+  }
+  // Mark this cell as visted
+  mazeArray[currRow][currCol] = true;
+
+  // Assemble random ordered-list of neighbours
+  const neighbors = shuffle([
+    [currRow - 1, currCol, "up"],
+    [currRow, currCol + 1, "right"],
+    [currRow + 1, currCol, "down"],
+    [currRow, currCol - 1, "left"],
+  ]);
+
+  for (let nbr of neighbors) {
+    const [nextRow, nextCol, direction] = nbr;
+
+    // Check for out of bounds
+    if (nextRow < 0 || nextRow >= row || nextCol < 0 || nextCol >= col) {
+      continue;
+    }
+    // Check if it has been visited
+    if (mazeArray[nextRow][nextCol]) {
+      continue;
+    }
+
+    // Remove a wall either vertical or horizontal
+    if (direction === "left") {
+      vertWalls[currRow][currCol - 1] = true;
+    } else if (direction === "right") {
+      vertWalls[currRow][currCol] = true;
+    } else if (direction === "up") {
+      horzWalls[currRow - 1][currCol] = true;
+    } else if (direction == "down") {
+      horzWalls[currRow][currCol] = true;
+    }
+    // visit next cell
+    stepThroughCell(nextRow, nextCol);
+  }
+};
 
 // run maze generation algo on mazeArray
 // ...
+const startRow = Math.floor(Math.random() * row);
+const startCol = Math.floor(Math.random() * col);
+stepThroughCell(startRow, startCol);
+
+console.log(mazeArray);
+console.log(vertWalls);
+console.log(horzWalls);
 
 // fill mazeArray with tile objs
 // ...
